e649b3ab4835af82a27f55b6181b1c0e5fca7e15
rozodkarbharat <99673832+rozodkarbharat@users.noreply.github.com>
2023-06-28 11:00:17 +0530

    pollyfill of map

---
+++ polyfills/index.js
@@ -0,0 +1,30 @@
+
+// Polyfill for Map 
+// Array.prototype.myMap = function(callback){
+//     let newArray=[]
+//     this.forEach((elemnt,index) => {
+//         newArray.push(callback(elemnt,index,this))
+//     })
+//     return newArray
+// }
+
+// let arr = [1,2,3,4,5,6,7,8,9,10]
+
+// console.log(arr.myMap(function(elemnt){
+//     return elemnt*2
+// }))
+
+var name ="bharat"
+
+let obj={
+    name :'n',
+    printname:function(){
+        let name='aish'
+        console.log(this.name)
+    },
+    pname:()=>{
+        console.log(this.name)
+    }
+}
+
+
