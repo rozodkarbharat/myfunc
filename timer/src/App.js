5b4ee35b719a76256d69d49c7f45ca4b2b765897
rozodkarbharat <99673832+rozodkarbharat@users.noreply.github.com>
2023-08-25 11:43:51 +0530

    code alignment changed

--- timer/src/App.js
+++ timer/src/App.js
@@ -1,27 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-import { useRef, useState } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import { useRef, useState } from "react";
 
 function App() {
-  const [time, settime] = useState(0)
-  const [isStaeted, setisStaeted] = useState(false)
-  const timer = useRef(null)
-  function handlestarttimer(){
-    if(!isStaeted)
-  { timer.current=setInterval(()=>{
-  settime((time)=>time+1)
-  },1000)
-  setisStaeted(true);
-}
+  const [time, settime] = useState(0);
+  const [isStarted, setisStarted] = useState(false);
+  const timer = useRef(null);
+  function handlestarttimer() {
+    if (!isStarted) {
+      timer.current = setInterval(() => {
+        settime((time) => time + 1);
+      }, 1000);
+      setisStarted(true);
+    }
   }
-  function handlecleartimer(){
-    clearInterval(timer.current)
-    settime(0)
-    setisStaeted(false)
+  function handlecleartimer() {
+    clearInterval(timer.current);
+    settime(0);
+    setisStarted(false);
   }
-  function handlestoptimer(){
-clearInterval(timer.current);
-setisStaeted(false);
+  function handlestoptimer() {
+    clearInterval(timer.current);
+    setisStarted(false);
   }
   return (
     <div className="App">
